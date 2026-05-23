3827bc1e85be9ba4f466d55b87edef2730f1503b
patelakhila20 <srinivaspatel720@gmail.com>
2026-05-23 10:04:09 +0530

    add new programs

---
+++ fun.py
@@ -0,0 +1,62 @@
+'''print("hi")
+input("Press Enter to exit....")'''
+
+#jumping
+#break
+'''for i in range(5,10):
+    print(i)
+    if(i==7):
+        break'''
+
+
+'''for i in range(2,7):
+    if(i==5):
+        break
+    print(i)'''
+
+'''for i in range(10,15):
+    print(i)
+    break'''
+
+'''a = 4 
+while(a<=7):
+    a = a+1
+    if(a==6):
+        break
+    print(a)'''
+
+'''for i in range (5):
+    break
+    print(i)'''
+
+'''for i in range(5,12):
+    if(i//2 == 4):
+        break
+    print(i)'''
+
+'''for i in range(5,14):
+    if(i//2 ==4):
+        break
+    print(i)'''
+
+#continue
+'''for i in range(5,10):
+    print(i)'''
+
+'''for i in range(5,10):
+    if (i ==7):
+        continue
+    print(i)'''
+'''for i in range (5,10):
+    print(i)
+    if(i == 7):
+        continue'''
+
+'''k = False
+m = float(k)
+print(m)'''
+
+a = 14.5642
+b = 153
+print("Hi%f"%a)
+print("bye%d"%b)
---
+++ srtings.py
@@ -0,0 +1,4 @@
+s1 = "akhila"
+s2 = "patel"
+s3 = s1+s2
+print(s3)
